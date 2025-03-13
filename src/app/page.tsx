8e57274f68739e7e2c9109d23dd6f998bec0d299
pokiji <110128494+Pokiji@users.noreply.github.com>
2025-03-13 11:28:02 -0700

    imgs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,30 +6,26 @@ export default function Home() {
     
     <div className="text-4xl self-center justify-self-center uppercase font-thin font-mono">Fent needle</div>
         <div className="grid grid-cols-2 md:grid-cols-3 gap-4 justify-items-center  ">
+            
+            <img className=" h-auto max-w-full rounded-3xl shadow-md hover:blur-lg"  src="https://placehold.co/350x400 " alt="placehold" />
 
+            <img className=" h-auto max-w-full rounded-3xl shadow-md hover:opacity-sky-700" src="https://placehold.co/350x250 " alt="placehold" />
 
+            <img className="h-auto max-w-full rounded-3xl shadow-md hover:opacity-sky-700"  src="https://placehold.co/350x350 " alt="placehold" />
 
+            <img className="h-auto max-w-full rounded-3xl shadow-md hover:opacity-sky-700" src="https://placehold.co/350x600 " alt="placehold" />
 
-            <img className="rounded-3xl shadow-md hover:blur-lg"  src="https://placehold.co/350x400 " alt="placehold" />
+            <img className="h-auto max-w-full rounded-3xl shadow-md hover:opacity-sky-700" src="https://placehold.co/350x300 " alt="placehold" />
 
-            <img className="rounded-3xl shadow-md hover:opacity-sky-700" src="https://placehold.co/350x250 " alt="placehold" />
+            <img className="h-auto max-w-full rounded-3xl shadow-md hover:opacity-sky-700" src="https://placehold.co/350x450 " alt="placehold" />
 
-            <img className="rounded-3xl shadow-md hover:opacity-sky-700"  src="https://placehold.co/350x350 " alt="placehold" />
+            <img className="h-auto max-w-full rounded-3xl shadow-md hover:opacity-sky-700" src="https://placehold.co/350x500  " alt="placehold" />
 
-            <img className="rounded-3xl shadow-md hover:opacity-sky-700" src="https://placehold.co/350x600 " alt="placehold" />
+            <img className="h-auto max-w-full rounded-3xl shadow-md hover:opacity-sky-700" src="https://placehold.co/350x500  " alt="placehold" />
 
-            <img className="rounded-3xl shadow-md hover:opacity-sky-700" src="https://placehold.co/350x300 " alt="placehold" />
+            <img className="h-auto max-w-full rounded-3xl shadow-md hover:opacity-sky-700" src="https://placehold.co/350x450   " alt="placehold" />
+          </div>
 
-            <img className="rounded-3xl shadow-md hover:opacity-sky-700" src="https://placehold.co/350x450 " alt="placehold" />
-
-            <img className="rounded-3xl shadow-md hover:opacity-sky-700" src="https://placehold.co/350x500  " alt="placehold" />
-
-            <img className="rounded-3xl shadow-md hover:opacity-sky-700" src="https://placehold.co/350x500  " alt="placehold" />
-
-            <img className="rounded-3xl shadow-md hover:opacity-sky-700" src="https://placehold.co/350x450   " alt="placehold" />
-
-
-        </div>
     </div>
   );
 }
